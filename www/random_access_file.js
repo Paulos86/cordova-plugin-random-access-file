a2c21b5e4e818b68a3646ffc68070ddae780758b
Paul Badenhorst <paul@catostylus.com>
2018-11-25 14:14:47 +0200

    plugin.xml foo

--- www/random_access_file.js
+++ www/random_access_file.js
@@ -3,7 +3,7 @@
 module.exports = {
     write: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
-    }
+    },
     read: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
     }
