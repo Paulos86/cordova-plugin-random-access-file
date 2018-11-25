55219e539d37073ec0e498de6a42088ed9f3d030
Paul Badenhorst <paul@catostylus.com>
2018-11-25 17:50:06 +0200

    foo

--- src/Android/Hello.java
+++ src/Android/Hello.java
@@ -3,6 +3,7 @@ package com.example.plugin;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 import java.io.File;
 import java.io.RandomAccessFile;
 
@@ -12,9 +13,10 @@ public class Hello extends CordovaPlugin {
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("greet")) {
-
-            File path = new File(data.getString(0))
-            callbackContext.success(path);
+            // File path = new File();
+            JSONObject file = data.getJSONObject(0);
+            JSONObject blob = data.getJSONObject(1);
+            callbackContext.success(data.getJSONObject(0).getClass().getSimpleName()+"|"+data.getJSONObject(1).getClass().getSimpleName());
             return true;
 
         } else {
